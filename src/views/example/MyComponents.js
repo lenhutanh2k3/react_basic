e377701377a3f787ac385b9819efe3ad18662d55
Le Nhut Anh <nhutanhle130403@gmail.com>
2025-02-20 22:01:21 +0700

    create file first component

---
+++ src/views/example/MyComponents.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MyComponents extends React.Component
+{
+    render()
+    {
+        return (
+            <p>XIn chao</p>
+        )
+    }
+}
+export default MyComponents
