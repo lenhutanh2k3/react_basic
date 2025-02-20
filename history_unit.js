48b3487e802280b5f48a60fc174277c2a7d25e36
Le Nhut Anh <nhutanhle130403@gmail.com>
2025-02-20 22:28:43 +0700

    State

---
+++ history_unit.js
@@ -0,0 +1,11 @@
+/**
+ * Bai 2: State
+ * Quản lý dữ liệu động: state lưu trữ những dữ liệu có thể thay đổi trong suốt vòng đời của component. Ví dụ, bạn có thể sử dụng state để lưu trữ trạng thái người dùng, dữ liệu từ server, hoặc trạng thái của một form.
+
+    Chỉ trong component: Mỗi component có thể có state riêng của nó, và dữ liệu trong state chỉ tồn tại trong component đó. Để chia sẻ dữ liệu giữa các component, bạn cần truyền dữ liệu thông qua props.
+
+    Cập nhật lại giao diện: Mỗi khi state thay đổi, React sẽ tự động re-render (vẽ lại) component để hiển thị dữ liệu mới. Điều này giúp đồng bộ giữa dữ liệu và giao diện một cách dễ dàng
+ * 
+ * 
+ * 
+ */
--- src/views/example/MyComponents.js
+++ src/views/example/MyComponents.js
@@ -1,12 +1,26 @@
 import React from 'react'
 
-class MyComponents extends React.Component
-{
-    render()
-    {
-        return (
-            <p>XIn chao</p>
-        )
-    }
+class MyComponents extends React.Component {
+  constructor(props) {
+    super(props);
+    // Khởi tạo state
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // Hàm tăng giá trị count
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
 }
 export default MyComponents
